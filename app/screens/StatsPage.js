c84392094e22a8763487c6df63c7510743e15d05
rose413 <irenechang949@gmail.com>
2024-02-17 15:24:37 -0800

    I moved assets and created StatsPage

---
+++ app/screens/StatsPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View, Image, SafeAreaView, Button, Pressable } from 'react-native';
+
+export default function StatsPage() {
+    return (
+        <View style = {styles.background}>
+            
+
+        </View>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    
+
+})
