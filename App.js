236fd9ef2e3f10eb20d10e354bc670ac54790a9f
TiffanyN22 <tnguyen22@ndsj.org>
2024-02-17 22:02:49 -0800

    geocoded location wip

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import TasksPage from './app/screens/tasks'
 // import Fetch from './src/Fetch'
 import AddTaskPage from "./app/screens/AddTask";
 import FriendsPage from "./app/screens/Friends"
+import InputLocation from "./src/InputLocation"
 
 const App = () => { 
-  return <AddTaskPage/>; 
+  return <InputLocation/>; 
 }; 
 
 export default App;
