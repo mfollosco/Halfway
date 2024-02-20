6c64f8decfa30f28af23430cdeedc66e49d96a54
mfollosco <mvfollosco@gmail.com>
2024-02-20 11:39:16 -0800

    commit fonts

---
+++ ogApp 2.js
@@ -0,0 +1,10 @@
+// import React, { useState } from "react"; 
+// import TasksPage from './app/screens/tasks' 
+// // import Fetch from './src/Fetch'
+// import AddTaskPage from "./app/screens/AddTask";
+
+// const App = () => { 
+//   return <AddTaskPage/>; 
+// }; 
+
+// export default App;
